73ab6886a39351d6d0448ab3f6745388dde5f082
Stephen Williams <steve@icarus.com>
2008-11-21 10:11:31 -0800

    Add simbus pci memory functions.
    The pcimem device deserves library functions to interface with it.
    These are based on similar functions in the pcisim project.

---
+++ libsimbus/simbus_pci_memdev.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2008 Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+# include  "simbus_pci.h"
+# include  "simbus_pci_memdev.h"
+# include  <stdlib.h>
+# include  <malloc.h>
+# include  <assert.h>
+
+
+# define FILL_CMD    0x0000
+# define COMPARE_CMD 0x0001
+# define LOAD_CMD    0x0002
+# define SAVE_CMD    0x0003
+
+struct simbus_pci_memdev_s {
+      simbus_pci_t bus;
+	/* Config space address for the device */
+      uint64_t config;
+	/* PCI Device ID */
+      uint32_t devid;
+      uint64_t base_memory;
+      uint64_t base_cntrl;
+};
+
+static void m1writel(struct simbus_pci_memdev_s*xsp, uint64_t off, uint32_t val)
+{
+      simbus_pci_write32(xsp->bus, xsp->base_cntrl + off, val, 0);
+}
+
+static uint32_t m1readl(struct simbus_pci_memdev_s*xsp, uint64_t off)
+{
+      return simbus_pci_read32(xsp->bus, xsp->base_cntrl + off, 0);
+}
+
+simbus_pci_memdev_t simbus_pci_memdev_init(simbus_pci_t bus, uint64_t config)
+{
+      uint32_t val;
+      struct simbus_pci_memdev_s*xsp = calloc(1, sizeof(struct simbus_pci_memdev_s));
+
+      xsp->bus = bus;
+      xsp->config = config;
+
+	/* We need the device ID to distinguish variants. */
+      xsp->devid = simbus_pci_config_read(xsp->bus, xsp->config + 0);
+
+      switch (xsp->devid) {
+
+	  case 0xffc112c5:
+	      /* BAR0 has low address bits */
+	    val = simbus_pci_config_read(xsp->bus, xsp->config+16);
+	    xsp->base_memory = val & ~15;
+	      /* BAR1 has high address bits */
+	    val = simbus_pci_config_read(xsp->bus, xsp->config + 20);
+	    xsp->base_memory = val;
+	    xsp->base_memory |= ((uint64_t)val) << 32;
+	      /* BAR2 has base of control registers */
+	    val = simbus_pci_config_read(xsp->bus, xsp->config + 24);
+	    xsp->base_cntrl = val & ~15;
+	    break;
+
+	  default:
+	    assert(0);
+	    break;
+      }
+
+      return xsp;
+}
+
+uint64_t simbus_pci_memdev_start(simbus_pci_memdev_t xsp)
+{
+      return xsp->base_memory;
+}
+
+void simbus_pci_memdev_fill(simbus_pci_memdev_t xsp,
+			    uint64_t off, unsigned cnt,
+			    uint32_t fill)
+{
+      m1writel(xsp,  4, off);
+      m1writel(xsp,  8, cnt);
+      m1writel(xsp, 12, fill);
+	/* This starts the command going. */
+      m1writel(xsp,  0, FILL_CMD);
+}
+
+int simbus_pci_memdev_compare(simbus_pci_memdev_t xsp,
+			      uint64_t off0, uint64_t off1,
+			      unsigned cnt0)
+{
+      unsigned long rc;
+      m1writel(xsp,  4, off0);
+      m1writel(xsp,  8, cnt0);
+      m1writel(xsp, 12, off1);
+      m1writel(xsp,  0, COMPARE_CMD);
+
+      rc = m1readl(xsp, 0);
+      return (rc == 0)? 0 : 1;
+}
+
+int simbus_pci_memdev_load(simbus_pci_memdev_t xsp,
+			   uint64_t offset, unsigned  count,
+			   const char*file)
+{
+      int rc;
+
+      m1writel(xsp,  4, offset);
+      m1writel(xsp,  8, count);
+
+      rc = 12;
+      while (file[0] != 0) {
+	    uint32_t word = 0;
+
+	    word |= ((uint32_t)file[0] <<  0) & 0x000000ffUL;
+	    if (file[0] != 0) file += 1;
+	    word |= ((uint32_t)file[0] <<  8) & 0x0000ff00UL;
+	    if (file[0] != 0) file += 1;
+	    word |= ((uint32_t)file[0] << 16) & 0x00ff0000UL;
+	    if (file[0] != 0) file += 1;
+	    word |= ((uint32_t)file[0] << 24) & 0xff000000UL;
+	    if (file[0] != 0) file += 1;
+
+	    m1writel(xsp, rc, word);
+	    rc += 4;
+      }
+
+      m1writel(xsp, rc, 0);
+
+      m1writel(xsp, 0, LOAD_CMD);
+
+      rc = m1readl(xsp, 0);
+      return rc;
+}
+
+int simbus_pci_memdev_save(simbus_pci_memdev_t xsp,
+			   uint64_t offset, unsigned count,
+			   const char*file)
+{
+      int rc;
+
+      m1writel(xsp,  4, offset);
+      m1writel(xsp,  8, count);
+
+      rc = 12;
+      while (file[0] != 0) {
+	    uint32_t word = 0;
+
+	    word |= (file[0] <<  0) & 0x000000ffUL;
+	    if (file[0] != 0) file += 1;
+	    word |= (file[0] <<  8) & 0x0000ff00UL;
+	    if (file[0] != 0) file += 1;
+	    word |= (file[0] << 16) & 0x00ff0000UL;
+	    if (file[0] != 0) file += 1;
+	    word |= (file[0] << 24) & 0xff000000UL;
+	    if (file[0] != 0) file += 1;
+
+	    m1writel(xsp, rc, word);
+	    rc += 4;
+      }
+
+      m1writel(xsp, rc, 0);
+
+      m1writel(xsp, 0, SAVE_CMD);
+
+      rc = m1readl(xsp, 0);
+      return rc;
+}
---
+++ libsimbus/simbus_pci_memdev.h
@@ -0,0 +1,94 @@
+#ifndef __simbus_pci_memdev_H
+#define __simbus_pci_memdev_H
+/*
+ * Copyright (c) 2008 Stephen Williams (steve@icarus.com)
+ *
+ *    This source code is free software; you can redistribute it
+ *    and/or modify it in source code form under the terms of the GNU
+ *    General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+
+/*
+ * The pci_memory module implements a PCI target memory region with
+ * some extra smarts. Once initialized, the device can be used as a
+ * memory target by other devices on the PCI bus.
+ *
+ *   Vendor ID: 0x12c5 (Picture Elements, Inc.)
+ *   Device ID: 0xffc1 (pci63_memory device)
+ */
+typedef struct simbus_pci_memdev_s *simbus_pci_memdev_t;
+
+#ifdef __cplusplus
+# define EXTERN extern "C"
+#else
+# define EXTERN extern
+#endif
+
+
+/*
+ * Given a config space base address, open the pcisim_memdev
+ * device. The config address depends on the system configuration,
+ * which attaches idsel signals for the device to the right
+ * places. The device should already have been mapped into the address
+ * space by whatever BIOS is being used.
+ */
+EXTERN simbus_pci_memdev_t simbus_pci_memdev_init(simbus_pci_t bus, uint64_t config);
+
+/*
+ * Get the base address of the main memory. This is the address
+ * programmed into BAR0, with the flags removed. This is the address
+ * that can be passed to other devices as pointers.
+ */
+EXTERN uint64_t simbus_pci_memdev_start(simbus_pci_memdev_t xsp);
+
+/*
+ * This function causes a region of memory to be filled with a given
+ * value. The "off" parameter is the offset into the memory area, and
+ * the cnt is the number of DWORDS to fill. The fill parameter is the
+ * value to write into the memory.
+ *
+ * The offset must be a multiple of 4, and cnt is the number of
+ * *DWORDS*, not bytes.
+ */
+EXTERN void simbus_pci_memdev_fill(simbus_pci_memdev_t xsp,
+				   uint64_t off, unsigned cnt,
+				   uint32_t fill);
+
+/*
+ * This function does a BYTE compare of two regions within the
+ * memory. The off0 and off1 arguments are the offsets within this
+ * memory of the regions to compare, and the cnt is the number of
+ * bytes to compare.
+ *
+ * This function returns 0 if the regions are identical.
+ */
+EXTERN int simbus_pci_memdev_compare(simbus_pci_memdev_t xsp,
+				     uint64_t off0, uint64_t off1,
+				     unsigned cnt);
+
+/*
+ * These commands tell the memory device to load/save data to/from a
+ * host file. The return code is the number of bytes of I/O, or a
+ * value <0 for an error.
+ */
+EXTERN int simbus_pci_memdev_load(simbus_pci_memdev_t xsp,
+				  uint64_t offset, unsigned count,
+				  const char*path);
+
+EXTERN int simbus_pci_memdev_save(simbus_pci_memdev_t xsp,
+				  uint64_t offset, unsigned count,
+				  const char*path);
+
+#endif
